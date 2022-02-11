1a51f4f983ce46727b72d0845a9f102aeac7f03d
chentommaiii <85600799+chentommaiii@users.noreply.github.com>
2022-02-11 18:28:27 +0800

    Complete desktop view 🔥

--- src/main.js
+++ src/main.js
@@ -10,3 +10,33 @@ $(".menu-icon").click(function() {
         $(".navbar").addClass("menu-open");
     }
   });
+
+  const reviewSlider = $(".review-slider");
+  const reviewLeftIcon = $('.review-carousel-btn-left');
+  const reviewRightIcon = $('.review-carousel-btn-right');
+
+  var sectionIndex = 0;
+
+    reviewLeftIcon.on('click', function() {
+        sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0
+        reviewSlider.css('transform','translate(' + (sectionIndex) * 50 + '%)');
+    })
+
+    reviewRightIcon.on('click', function() {
+        sectionIndex = (sectionIndex < 1) ? sectionIndex + 1 : 1
+        reviewSlider.css('transform','translate(' + (sectionIndex) * -50 + '%)');
+    })
+
+const clientSlider = $(".client-slider");
+const clientLeftIcon = $('.client-carousel-btn-left');
+const clientRightIcon = $('.client-carousel-btn-right');
+
+const clientCarouselWidth = $(".client-carousel").width();
+
+clientLeftIcon.on('click', function() {
+    clientSlider.css('transform','translateX(0)');
+})
+
+clientRightIcon.on('click', function() {
+    clientSlider.css('transform','translateX(' + -clientCarouselWidth + 'px)');
+})
