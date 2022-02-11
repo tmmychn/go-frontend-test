b781867f8a1a62ebdaae8bc7d15f989fc7e86c22
chentommaiii <85600799+chentommaiii@users.noreply.github.com>
2022-02-11 09:54:21 +0800

    Initial commit ✨

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import './style.css';
+import './fonts.css';
+
+import leftshape from './assets/shape_left.png';
+import rightshape from './assets/shape_right.png';
+import sectionbreak1 from './assets/section-icon_2.png';
+import sectionbreak from './assets/section-icon.png';
+import apdlogo from './assets/apd-logo.png';
+import strengthicon from './assets/icon_4.png';
+import image from './assets/image.png'
+import scenthound from './assets/scenthound-logo.png'
+import vividways from './assets/vividways-logo.png'
+import hobnob from './assets/hobnob-logo.png'
+import burnabook from './assets/burnabook-logo.png'
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template:"./src/template.html"
+  })],
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  module: {
+      rules:[
+          {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+          },
+          {
+                test: /\.(js)$/,
+                use: ['babel-loader'],
+          },
+          {
+              test: /\.(png|svg|jpg|jpeg|gif)$/,
+              use: {
+                  loader: "file-loader",
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/'
+                  }
+              },
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+                }
+              }
+            ]
+          },
+      ],
+  },
+};
